b543e6d40a8582acf8c5b87295506af52a13e48f
fang <fangtoby@live.cn>
2016-11-18 14:03:27 +0800

    Create tcp_flood.c

---
+++ tcp_flood.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <linux/tcp.h>
+
+int main(int argc,char** argv){
+  printf("tcp attack start...");
+  exit(1);
+}
