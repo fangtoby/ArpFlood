70e3443a1fa8d34d7be804b96bafb155520e638a
fang <fangtoby@live.cn>
2016-11-18 16:24:40 +0800

    Create scanMyMac.c

---
+++ scanMyMac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+
+int main(int argc, char **argv) {
+
+}
