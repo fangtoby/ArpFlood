318b12fac29ad6244cc2fbed06f39a13f7c6b253
fang <fangtoby@live.cn>
2016-11-21 19:01:23 +0800

    Update scanMyMac.c

--- linux/scanMyMac.c
+++ linux/scanMyMac.c
@@ -158,4 +158,13 @@ void arp_packet_callback(char * packet_content)
 	printf("    |-Hardware Address Length:%d\n",arpst->ar_hln);
 	printf("    |-Protocol Address Length:%d\n",arpst->ar_pln);
 	printf("    |-ARP Opcode             :%d\n",ntohs(arpst->ar_op));
+	/* ARP protocol opcodes. */  
+	//#define     ARPOP_REQUEST    1                /*ARP request                            */  
+	//#define     ARPOP_REPLY 2                /*ARP reply                        */  
+	//#define     ARPOP_RREQUEST  3                /*RARP request                         */  
+	//#define     ARPOP_RREPLY        4                /*RARP reply                     */  
+	//#define     ARPOP_InREQUEST 8                /*InARP request               */  
+	//#define     ARPOP_InREPLY       9                /*InARP reply                    */  
+	//#define     ARPOP_NAK     10              /*(ATM)ARP NAK                       */  
+	
 }
